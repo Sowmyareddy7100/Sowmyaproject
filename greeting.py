63f4514fe13adabc7f696758ad544e5ce3103657
Sowmyareddy7100 <your-email@example.com>
2025-04-05 19:15:16 -0400

    Second Commit

--- greeting.py
+++ greeting.py
@@ -1,6 +1,6 @@
 def greet(name):
   """Greets the person passed in as a parameter."""
-  print(f"Hello, {name}! How are you today?")
+  print(f"Hello, {name}! How are you today?")The time is now 7:07 PM in Durham, North Carolina.")
 
 if __name__ == "__main__":
   user_name = input("Enter your name: ")
